30a201e3b420cda7fc547fb014482f72568af093
sloan58 <martin.sloan@karma-tek.com>
2019-11-15 12:37:10 -0500

    first commit

---
+++ app/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadFile(`${__dirname}/index.html`);
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
